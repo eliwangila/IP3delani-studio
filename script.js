b3efff7cad21106cbd427e1199edd23a2dcaafe8
eliwangila <ekirapaeli254@gmail.com>
2021-07-25 14:01:47 +0300

    add hover effect to the image

--- script.js
+++ script.js
@@ -32,4 +32,67 @@
           $("#product-image").slideDown('1500');
         });
       });
+                  // portfolio section mouseenter the image hide and show the hover effect
+            //image 
+
+            $(document).ready(function(){
+                $("#work").mouseover(function(){
+                $("#mouseenter").show();
+                 });
+                $("#work").mouseout(function(){
+                $("#mouseenter").hide();
+                });
+              //image 2
+          
+              $("#work1").mouseover(function(){
+                  $("#mouseenter1").show();
+                })
+                $("#work1").mouseout(function(){
+                  $("#mouseenter1").hide();
+                });
+                //image 3
+          
+                $("#work2").mouseover(function(){
+                  $("#mouseenter2").show();
+                })
+                $("#work2").mouseout(function(){
+                  $("#mouseenter2").hide();
+                });
+                //image 4
+                $("#work3").mouseover(function(){
+                  $("#mouseenter3").show();
+                })
+                $("#work3").mouseout(function(){
+                  $("#mouseenter3").hide();
+                });
+                //image 5
+                $("#work4").mouseover(function(){
+                  $("#mouseenter4").show();
+                })
+                $("#work4").mouseout(function(){
+                  $("#mouseenter4").hide();
+                });
+                  //image 6
+                $("#work5").mouseover(function(){
+                  $("#mouseenter5").show();
+                })
+                $("#work5").mouseout(function(){
+                  $("#mouseenter5").hide();
+                });
+                  //image 7
+                $("#work6").mouseover(function(){
+                  $("#mouseenter6").show();
+                })
+                $("#work6").mouseout(function(){
+                  $("#mouseenter6").hide();
+                });
+                //image 8
+                $("#work7").mouseover(function(){
+                  $("#mouseenter7").show();
+                })
+                $("#work7").mouseout(function(){
+                  $("#mouseenter7").hide();
+                });
+            });
+          
          
